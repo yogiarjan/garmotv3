91d213ac56cac6375b7925525a296c0d6dfa57e9
Yogi Arjan <yogiarjan@gmail.com>
2012-05-21 18:57:38 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garmotv3::Application.config.secret_token = '669aa0a55a74445e040ccdc823c718c649b2be082a6a9ece8b8302b9871d7b70ae3e4c0dab309b394d79153be2bbf9f4288d8fb0c4d0059a88f27d0ae728b3e5'
